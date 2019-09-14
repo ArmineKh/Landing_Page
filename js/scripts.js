4402a211da4d3019c1013e3ec47680d1ab767c93
Armine <https://github.com/ArmineKh>
2019-09-15 00:25:23 +0400

    Added events-form section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(function () {
-  $('#myTab li:last-child a').tab('show')
+  $('#myTab li:last-child a').tab('show');
 });
+$(function(){
+	$('.collapse').on('show.bs.collapse', function(){
+		$(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+	});
+	$('.collapse').on('hide.bs.collapse', function(){
+		$(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+	});
+	$('#myForm').validator({
+		feedback: {
+			success: 'fa fa-check-circle',
+			error: 'fa fa-times-circle'
+		}
+	});
+});
+
+
